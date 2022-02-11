5ef07e8aa990b51c09bd688f3e36e220bfb16c4e
Joannarama <joanne@bolgers.net>
2022-02-11 16:03:38 +0000

    implement javascript for user errors and question counter

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,13 @@ let questionInput = document.getElementById("question");
 
 let ballResponse = document.getElementById("answer");
 
+let questionError = document.getElementById('question-error');
+
+let questionContainer = new Array();
+
+let remainingQuestions = document.getElementById('remaining-questions');
+let startOver = document.getElementById('start-over');
+
 let options = [
     "Don't count your chickens",
     "What do YOU think?",
@@ -29,12 +36,50 @@ let options = [
   ];
 
 askButton.addEventListener("click", function(){
+    questionError.innerText = '';
+    questionError.style.display = 'none';
+
     if (questionInput.value.length < 1) {
-      alert("Please enter a question!");
+        questionError.style.display = "block";
+        questionError.innerText = "You must ask a question!";
+    } else if (questionInput.value.indexOf('?') == -1) {
+        questionError.style.display = "block";
+        questionError.innerText = "Are you sure you asked a question? Questions usually end with a question mark!";        
     } else {
-        let num = questionInput.value.length % options.length;
-        ballResponse.innerText = options[num]
-      }
+        for(i=0; i < questionContainer.length; i++) {
+            if (questionContainer[i] == questionInput.value) {
+                questionError.style.display = "block";
+                questionError.innerText = "The Oracle has already answered that question. Don't waste her time!"; 
+
+                return;
+            }
+        }
+
+
+        let index = getRandomQuestionIndex(0, options.length - 1);
+        ballResponse.innerText = options[index]
+
+        questionContainer.push(questionInput.value)
+
+        updateRemainingQuestions();
+    }
+
   });
 
+
+  startOver.addEventListener('click', function() {
+     questionContainer = new Array();
+     updateRemainingQuestions();
+     questionInput.value = '';
+     
+  });
+
+  function updateRemainingQuestions() {
+    let totalRemaining = 3 - questionContainer.length;
+    remainingQuestions.innerText = 'Remaining Questions: ' + totalRemaining;
+  }
+
+  function getRandomQuestionIndex(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+  }
 
